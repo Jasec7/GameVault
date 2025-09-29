6c200fd064afa14ab00892537060829dadd45af9
Jasec <jasec_7@hotmail.com>
2025-09-29 01:45:01 -0700

    add more helpers

--- lib/helpers.py
+++ lib/helpers.py
@@ -9,4 +9,17 @@ def exit_program():
 def list_consoles():
     consoles = Console.get_all()
     for console in consoles:
-        print(console) 
+        print(f'{console.id}. {console.name}')
+
+def find_console_by_id():
+    id_ = input("Enter the console's id:").strip()
+    console = Console.find_by_id(id_)
+    print(console) if console else print(f'Console {id_} not found')
+
+def create_console():
+    name = input("Enter the console's name:")
+    try:
+        console = Console.create(name)
+        print(f"Success: {console.name}")
+    except ValueError as exc:
+        print("Error creating console:", exc)
