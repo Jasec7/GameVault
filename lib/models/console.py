846bf1988e8e8f8ac51e39466a0621d0da53e3b3
Jasec <jasec_7@hotmail.com>
2025-09-29 01:51:08 -0700

    create find_by_name in console.py

--- lib/models/console.py
+++ lib/models/console.py
@@ -121,6 +121,17 @@ class Console:
         """
         row = CURSOR.execute(sql,(id,)).fetchone()
         return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_name(cls, name):
+        """Return Console object corresponding to first table row matching specified name"""
+        sql = """
+            SELECT *
+            FROM consoles
+            WHERE name is ?       
+        """
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
 
 
