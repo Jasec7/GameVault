2e5676635ffdf904d1cdf30052134a4420f959f0
Jasec <jasec_7@hotmail.com>
2025-10-01 03:07:24 -0700

    modify cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -17,54 +17,47 @@ from helpers import (
 
 def main():
     while True:
-        menu()
-        choice = input("> ").strip()
+        print("---- Main Menu ----")
+        print("1. Consoles")
+        print("2. Games")
+        print("0. Exit")
+
+        choice = input("> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
-            list_consoles()
+            consoles_menu()
         elif choice == "2":
+            games_menu()
+
+
+def consoles_menu():
+    while True:
+        print("---- Consoles Menu ----")
+        print("1. List all consoles")
+        print("2. Find console by name")
+        print("3. Find console by id")
+        print("4. Create console")
+        print("5. Update console")
+        print("6. Delete console")
+        print("0. Back to main menu")
+
+        choice = input("> ").strip()
+        if choice == "0":
+            break
+        elif choice == "1":
+            list_consoles()
+        elif choice == "2": 
             find_console_by_name()
-        elif choice == "3":
+        elif choice == "3": 
             find_console_by_id()
-        elif choice == "4":
+        elif choice == "4": 
             create_console()
-        elif choice == "5":
+        elif choice == "5": 
             update_console()
-        elif choice == "6":
+        elif choice == "6": 
             delete_console()
-        elif choice == "7":
-            list_games()
-        elif choice == "8":
-            find_game_by_name()
-        elif choice == "9":
-            find_game_by_id()
-        elif choice == "10":
-            create_game()
-        elif choice == "11":
-            update_game()
-        elif choice == "12":
-            delete_game()
         else:
-            print("Invalid choice")
-
-
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. List all consoles")
-    print("2. Find console by name")
-    print("3. Find consolet by id")
-    print("4. Create console")
-    print("5. Update console")
-    print("6. Delete console")
-    print("7. List all games")
-    print("8. Find game by name")
-    print("9. Find game by id")
-    print("10. Create game")
-    print("11. Update game")
-    print("12. Delete game")
-
-
+            print("Invalid choice, please try again.")
 if __name__ == "__main__":
     main()
