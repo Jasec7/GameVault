4a2eff835c0382f508d9acd6ed745bcbb1f91ce5
Jasec <jasec_7@hotmail.com>
2025-10-04 02:49:01 -0700

    modify CLI display

--- lib/cli.py
+++ lib/cli.py
@@ -24,16 +24,17 @@ def main():
     while True:
         print(Fore.GREEN + "Welcome to GameVault!")
         print(Fore.CYAN + "*** Main Menu ***")
-        print(Fore.YELLOW +"1. Consoles")
-        print(Fore.YELLOW +"2. Games")
-        print(Fore.YELLOW +"0. Exit")
+        print(Fore.YELLOW +"Type the letter from the following options:")
+        print(Fore.YELLOW +"C. Consoles")
+        print(Fore.YELLOW +"G. Games")
+        print(Fore.YELLOW +"E. Exit")
 
-        choice = input("> ")
-        if choice == "0":
+        choice = input("> ").strip().lower()
+        if choice == "e":
             exit_program()
-        elif choice == "1":
+        elif choice == "c":
             consoles_menu()
-        elif choice == "2":
+        elif choice == "g":
             games_menu()
         else: 
              print(Fore.RED +"Invalid choice, please try again.")
@@ -46,9 +47,6 @@ def consoles_menu():
         print(Fore.CYAN + "*** Consoles Menu ***")
         print(Fore.YELLOW +"1. List all consoles")
         print(Fore.YELLOW +"2. Find console by name")
-        print(Fore.YELLOW +"3. Create console")
-        print(Fore.YELLOW +"4. Update console")
-        print(Fore.YELLOW +"5. Delete console")
         print(Fore.YELLOW +"0. Back to main menu")
 
         choice = input("> ").strip()
@@ -58,12 +56,6 @@ def consoles_menu():
             consoles_screen_menu()
         elif choice == "2": 
             find_console_by_name()
-        elif choice == "3": 
-            create_console()
-        elif choice == "4": 
-            update_console()
-        elif choice == "5": 
-            delete_console()
         else:
             print(Fore.RED +"Invalid choice, please try again.")
 
@@ -79,14 +71,15 @@ def consoles_screen_menu():
 
 
         print(Fore.CYAN + "*** Console List Screen ***")
-        print(Fore.YELLOW +"N. Add new console U <#> Update a console B. Back to Consoles Menu")
+        print(Fore.YELLOW +"Select a letter from the following options:")
+        print(Fore.YELLOW +"A. Add new console U <#>. Update a console B. Back to Consoles Menu")
        
         choice = input("> ").strip().lower()
         if choice == "b":
            break
-        elif choice == "n":
+        elif choice == "a":
             create_console()
-            
+
         else:
             parts = choice.split()
         
@@ -94,7 +87,9 @@ def consoles_screen_menu():
                 try:
                     idx = int(parts[1])
                     selected_console = consoles[idx - 1]
-                    print(f"You selected {selected_console.name} to update")
+                    #print(f"You selected {selected_console.name} to update")
+                    #print(f"Updating: {selected_console.name} (id {selected_console.id})")
+                    update_console()
             
                 except (ValueError, IndexError):
                     print(Fore.RED + "Invalid number. Try again.")
